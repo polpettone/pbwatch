c9357801048b3462281af8abf775b9067befb8e2
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-11-03 21:07:38 +0100

    edit

---
+++ cmd/editCmd.go
@@ -0,0 +1,70 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+	"time"
+)
+
+func (app *Application) NewEditCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "edit",
+		Short: "",
+		Long:  ``,
+
+		Run: func(command *cobra.Command, args []string) {
+			app.handleEditCommand(args)
+		},
+	}
+}
+
+func (app *Application) handleEditCommand(args []string) {
+	repo := NewRepo(app.Logging,
+		app.DBPort,
+		app.DBUser,
+		app.DBPassword,
+		app.DBName)
+
+	date, err := time.ParseInLocation("02.01.2006", args[0], time.Local)
+
+	if err != nil {
+		app.Logging.Stdout.Printf("Could not parse date %v", err)
+	}
+
+	stat, err := repo.findStatByDate(DateTime{date})
+
+	if err != nil {
+		app.Logging.Stdout.Printf("Could not find stat %v", err)
+	}
+
+	yamlStat, err := yaml.Marshal(stat)
+	result, err := CaptureInputFromEditor(string(yamlStat))
+
+	if err != nil {
+		app.Logging.ErrorLog.Printf("%v", err)
+		return
+	}
+
+	editedStat := &Stat{}
+
+	err = yaml.Unmarshal([]byte(result), &editedStat)
+	if err != nil {
+		app.Logging.ErrorLog.Printf("%v", err)
+		return
+	}
+
+	err = repo.saveStat(editedStat)
+
+	if err != nil {
+		app.Logging.ErrorLog.Printf("%v", err)
+		return
+	}
+
+}
+
+func init() {
+	logging := NewLogging()
+	app := NewApplication(logging)
+	editCmd := app.NewEditCmd()
+	rootCmd.AddCommand(editCmd)
+}
